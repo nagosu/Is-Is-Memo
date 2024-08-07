b3b1aefc09df29b357b53407176f09fe73298dcb
nagosu <nozick1021@gmail.com>
2024-08-07 18:42:25 +0900

    HeaderMain 퍼블리싱

--- src/components/HeaderMain/HeaderMain.tsx
+++ src/components/HeaderMain/HeaderMain.tsx
@@ -1,7 +1,9 @@
 function HeaderMain() {
   return (
-    <header className="fixed left-0 top-0 flex h-14 w-full bg-teal-600">
-      <div></div>
+    <header className="fixed left-0 top-0 flex h-14 w-full items-center justify-between bg-teal-600 px-8">
+      <a href="/" className="text-2xl font-bold text-white">
+        It Is <span className="text-green-400">Memo</span>
+      </a>
     </header>
   );
 }
